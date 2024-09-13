23d4e46e5a2a93546f7fc355f39123e3a48934f7
Daniel-Dief <danieldiefsantos@gmail.com>
2024-09-12 23:06:58 -0300

    feat: Criação do botão "Iniciar"

---
+++ src/components/StartButton/styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 12px;
+    border-radius: 24px;
+    border: none;
+    background-color: #00000088;
+    width: calc(100% / 3 * 2);
+`
+
+export const Text = styled.p`
+    font-family: Inter;
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 17.6px;
+    text-align: center;
+    color: #FFF;
+`
