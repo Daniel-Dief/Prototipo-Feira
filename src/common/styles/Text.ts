eda98940d0c4fed6d5687dc0577a281d51c4aee1
Daniel-Dief <danieldiefsantos@gmail.com>
2024-09-12 21:44:31 -0300

    feat: Adição dos estilos de textos

---
+++ src/common/styles/Text.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const Welcome = styled.h1`
+    font-family: Inter, sans-serif;
+    font-size: 32px;
+    font-weight: 700;
+    line-height: 38.73px;
+    text-align: center;
+    color: #FFF;
+`;
+
+export const SmallText = styled.p`
+    font-family: Inter, sans-serif;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 14.52px;
+    text-align: center;
+    color: #71717A;
+`;
+
+export const Label = styled.label`
+    font-family: Inter;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 15.4px;
+    text-align: center;
+    color: #000;
+`;
+
+export const Title = styled.h1`
+    font-family: Inter;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 24.2px;
+    text-align: left;
+    color: #18181B;
+`;
+
+export const SpanText = styled.span`
+    font-family: Inter;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 16.94px;
+    text-align: left;
+    color: #C2AE8F;
+`;
+
+export const Link = styled.a`
+    font-family: Inter;
+    font-size: 13px;
+    font-weight: 600;
+    line-height: 20px;
+    text-align: left;
+    color: #C2AE8F;
+`;
