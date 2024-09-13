3fb39a0fc53a4dfb1c1888d8881de26765c7095e
Daniel-Dief <danieldiefsantos@gmail.com>
2024-09-12 22:28:05 -0300

    feat: Styles dos icones

---
+++ src/common/styles/Imgs.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const BigIcon = styled.img`
+    width: 24px;
+    height: 24px;
+`
+
+export const Icon = styled.img`
+    width: 20px;
+    height: 20px;
+`
+
+export const SmallIcon = styled.img`
+    width: 16px;
+    height: 16px;
+`
+
+export const Logo = styled.img`
+    width: 48px;
+    height: 48px;
+`
+
+export const Flag = styled.img`
+    width: 32px;
+    height: 20px;
+    border-radius: 4px;
+`
