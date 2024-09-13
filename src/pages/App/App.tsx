d35faf5c4954b527c81f16dc0bc921ffee7e2ada
Daniel-Dief <danieldiefsantos@gmail.com>
2024-09-13 20:59:03 -0300

    feat: Preparando as rotas para as pages

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,20 +1,18 @@
-import Input from "../../components/inputText/index";
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Login from "./Login";
+
+import Welcome from "../Welcome";
+import SignIn from "../SignIn";
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <p><Link to="/">Teste</Link></p>
-          <p><Link to="/Login">Teste</Link></p>
-        </nav>
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="*" element={<Welcome />} />
+        <Route path="/Welcome" element={<Welcome />} />
+        <Route path="/SignIn" element={<SignIn />} />
+      </Routes>
     </Router>
-)}
+  )
+}
 
 export default App;
