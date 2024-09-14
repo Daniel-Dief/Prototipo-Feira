cc96635e817cecb69a11b53ac031d575c0ff01c1
Daniel-Dief <danieldiefsantos@gmail.com>
2024-09-13 21:09:34 -0300

    refactor: Verificando usuário para locar ou cadastrar

--- src/components/StartButton/index.tsx
+++ src/components/StartButton/index.tsx
@@ -1,10 +1,13 @@
+import { useEffect } from "react";
 import { Empty } from "../../common/styles/Empty";
 import { Icon } from "../../common/styles/Imgs";
 import { ButtonContainer, Text } from "./style";
 
 export default function StartButton() {
+    const path : string = localStorage.getItem("user") ? "/SignUp" : "/SignIn";
+
     return (
-       <ButtonContainer to="SignIn">
+       <ButtonContainer to={path}>
             <Empty />
             <Text>Iniciar</Text>
             <Icon src={require("../../common/images/rightArrow.png")} />
