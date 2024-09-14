2716ecbc01b67625191d795016a04e4cf3b5ad74
Daniel Santos <danieldiefsantos@gmail.com>
2024-09-14 17:04:16 -0300

    feat: Estilização de componentes

---
+++ src/common/styles/Checkbox.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Checkbox = styled.input`
+    width: 20px;
+    height: 20px;
+    padding: 2px;
+    border-radius: 4px;
+
+    &:checked {
+        background-color: #C2AE8F;
+    }
+`;
