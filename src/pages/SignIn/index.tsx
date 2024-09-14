5f32f6f940ce5e137276777043638a73c01e2f42
Daniel Santos <danieldiefsantos@gmail.com>
2024-09-14 17:06:11 -0300

    feat: Criação de SignIn Page

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,83 @@
+import { useRef } from "react";
+import { Button, ButtonCreateAccount } from "../../common/styles/Button";
+import { Form } from "../../common/styles/Form";
+import { Logo } from "../../common/styles/Imgs";
+import { Title, SmallText, SpanText } from "../../common/styles/Text";
+import Fild from "../../components/Fild";
+import CheckBox from "../../components/CheckBox";
+
+import { Container, Header, DivTitle, CreateAccount, AuxDiv } from "./style";
+
+export default function SignIn() {
+    const formRef = useRef<HTMLFormElement>(null);
+
+    function handleSubmit(event: React.FormEvent) {
+        event.preventDefault();
+        window.location.href = '/Home';
+    };
+
+    function handleButtonClick() {
+        formRef.current?.dispatchEvent(new Event('submit'));
+    }
+
+    function handleCreateAccount() {
+        window.location.href = '/SignUp';
+    }
+
+    return (
+    <Container>
+        <Header>
+            <Logo src={require("../../common/images/logoBlack.png")} />
+            <DivTitle>
+                <Title>
+                    Bem vindo!
+                </Title>
+                <SmallText>
+                    Faça o login para continuar
+                </SmallText>
+            </DivTitle>
+        </Header>
+        <Form
+            ref={formRef}
+            onSubmit={handleSubmit}
+        >
+            <Fild 
+                labelText="Email"
+                type="email"
+                requiered={true}
+                placeholder="Digite seu email"
+            />
+            <Fild 
+                labelText="Senha"
+                type="password"
+                requiered={true}
+                placeholder="Digite sua senha"
+            />
+            <CheckBox 
+                labelText="Lembrar de mim"
+                spanText="Esqueceu sua senha?"
+            />
+            <Button
+                onClick={handleButtonClick}
+            >
+                Entrar
+            </Button>
+        </Form>
+        <CreateAccount>
+            <AuxDiv>
+                <SmallText>
+                    Não tem uma conta ainda?
+                </SmallText>
+                <ButtonCreateAccount
+                    onClick={handleCreateAccount}
+                >
+                    Criar conta
+                </ButtonCreateAccount>
+            </AuxDiv>
+            <SpanText>
+                Termos e condições
+            </SpanText>
+        </CreateAccount>
+    </Container>
+    );
+}
---
+++ src/pages/SignIn/style.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 48px;
+  height: 100vh;
+  width: 85%;
+  margin: auto;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 48px 0;
+    gap: 24px;
+    width: 85%;
+`;
+
+export const DivTitle = styled.div`
+    width: 100%;
+`;
+
+export const CreateAccount = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    flex-direction: column;
+    gap: 16px;
+    margin-top: 24px;
+`;
+
+export const AuxDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    flex-direction: column;
+`;
