79ac34a37456d8c75107e58e60984d5be178f943
Daniel-Dief <danieldiefsantos@gmail.com>
2024-09-13 22:56:47 -0300

    feat: Criação da pagina SignIn

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,49 @@
+import { useState } from "react"
+
+import { Title, SmallText } from "../../common/styles/Text"
+import { Logo } from "../../common/styles/Imgs"
+import CodeVerify from "../../components/Input/CodeVerify"
+import FormButton from "../../components/Buttons/FormButton"
+
+import { Container, Header } from "./styled"
+
 export default function SignIn() {
+    const [inputValue, setInputValue] = useState<string | "">("");
+
+    function handlePath() {
+        window.location.href = "/Welcome";
+    }
+
+    function handleInput(value: string) {
+        if(inputValue.length >= 5){
+            setInputValue(inputValue.slice(0, 4) + value);
+        } else {
+            setInputValue(inputValue + value);
+        }
+    }
+
     return (
-        <h1>Sign In</h1>
+        <Container>
+            <Header>
+                <Logo src={require("../../common/images/logoBlack.png")} />
+                <div>
+                    <Title>
+                        Código de acesso
+                    </Title>
+                    <SmallText>
+                        Use o código do hotel para desbloquear sua jornada Orquestre.
+                    </SmallText>
+                </div>
+            </Header>
+            <CodeVerify 
+                setInputValue={handleInput}
+            />
+            <FormButton
+                onClick={handlePath}
+                disabled={inputValue.length < 5}
+            >
+                Verificar
+            </FormButton>
+        </Container>
     )
 }
---
+++ src/pages/SignIn/styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 48px;
+  height: 100vh;
+  width: 85%;
+  margin: auto;
+`;
+
+export const Header = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 48px 0;
+    gap: 24px;
+    width: 85%;
+`;
