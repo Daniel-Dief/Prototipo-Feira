1175c448a6d4dffc3c130116eabda2d9c55f9fff
Daniel Santos <danieldiefsantos@gmail.com>
2024-09-16 12:11:24 -0300

    feat: Criação do componente ShortInfo

---
+++ src/components/ShortInfo/index.tsx
@@ -0,0 +1,22 @@
+import { Icon } from '../../common/styles/Imgs';
+import { SmallText } from '../../common/styles/Text';
+
+import { Container } from './style';
+
+interface ShortInfoProps {
+    icon: string;
+    text: string;
+}
+
+export default function ShortInfo({ icon, text }: ShortInfoProps) {
+    return (
+        <Container>
+            <Icon src={icon} />
+            <SmallText
+                color="#C2AE8F"
+            >
+                {text}
+            </SmallText>
+        </Container>
+    )
+}
---
+++ src/components/ShortInfo/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 28px;
+    padding: 4px 8px 4px 8px;
+    border-radius: 12px;
+    background-color: #F9F7F3;
+    
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 4px;
+`;
