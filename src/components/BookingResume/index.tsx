dd9cfaedb58d26343239cb36a6dc480533191cd4
Daniel Santos <danieldiefsantos@gmail.com>
2024-09-17 15:06:47 -0300

    feat: criação do componenet booking resume

---
+++ src/components/BookingResume/index.tsx
@@ -0,0 +1,55 @@
+import { Container, Divider } from '../../common/styles/Divs'
+import { Title, SmallText, WhiteSpanText } from '../../common/styles/Text'
+import { Icon } from '../../common/styles/Imgs';
+
+import { HotelImage, DivTop, DivText, DivBottom, InfoDiv } from './style';
+
+export default function BookingResume() {
+    return (
+        <Container
+            gap='12px'
+            style={{
+                backgroundColor: "#FFF",
+                padding: "12px",
+                borderRadius: "12px",
+        }}>
+            <DivTop>
+                <HotelImage src={require("../../common/images/rectangle2.png")}/>
+                <DivText>
+                    <Title>
+                        Suíte Montanhosa
+                    </Title>
+                    <p
+                        style={{
+                            textAlign: "left",
+                            width: "100%",
+                            color: "#000"
+                        }}
+                    >
+                        A 123 / 2°
+                    </p>
+                </DivText>
+            </DivTop>
+            <Divider />
+            <DivBottom>
+                <InfoDiv>
+                    <SmallText>Check-in</SmallText>
+                    <WhiteSpanText
+                        color="#000"
+                    >
+                        Seg., 15 de jun
+                    </WhiteSpanText>
+                </InfoDiv>
+                <InfoDiv>
+                    <SmallText>Check-in</SmallText>
+                    <WhiteSpanText
+                        color="#000"
+                    >
+                        Sab., 20 de jun
+                    </WhiteSpanText>
+                </InfoDiv>
+                <Icon src={require("../../common/images/rightArrowBrown.png")}/>
+            </DivBottom>
+        </Container>
+    )
+}
---
+++ src/components/BookingResume/style.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const HotelImage = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 12px;
+`;
+
+export const DivTop = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    width: 100%;
+    gap: 12px;
+`;
+
+export const DivText = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    flex-direction: column;
+`;
+
+export const DivBottom = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 12px;
+`;
+
+export const InfoDiv = styled.div`
+    display: flex;
+    align-items: start;
+    justify-content: center;
+    flex-direction: column;
+`;
